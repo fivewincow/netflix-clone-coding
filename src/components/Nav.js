d71017893a1b55c86f2573e17328d01d52d8457e
fivewincow <fivewincow@gmail.com>
2022-08-03 23:58:53 +0900

    Movie Search

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,6 +21,11 @@ const Nav = () => {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"} `}>
       <img
@@ -26,6 +34,14 @@ const Nav = () => {
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+
       <img
         alt="User logged"
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
 
-export default SearchPage
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  console.log("searchTerm ", searchTerm);
+  return <div>SearchPage</div>;
+};
+
+export default SearchPage;
