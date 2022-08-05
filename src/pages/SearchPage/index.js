9f3ad660da04a67d5d7325bfa943e4b01f186d7b
seungwoo-gn <seungwoo-gn@genesisnest.com>
2022-08-05 23:57:38 +0900

    useOnClickOutside

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 import "./SearchPage.css";
 import useDebounce from "../../hooks/useDebounce";
 
 const SearchPage = () => {
+  const navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -41,7 +42,10 @@ const SearchPage = () => {
               "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
             return (
               <div className="movie" key={movie.id}>
-                <div className="movie__column-poster">
+                <div
+                  onClick={() => navigate(`/${movie.id}`)}
+                  className="movie__column-poster"
+                >
                   <img
                     src={movieImageUrl}
                     alt="movie"
